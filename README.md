c94ce5f8245b697f9e2a39405ec48af740c24902
lixinxin <251089003@qq.com>
2015-07-13 14:10:40 +0800

    在LDrawer基础上起一个好玩的项目

---
+++ README.md
@@ -0,0 +1,125 @@
+LDrawer
+=======
+
+[![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-LDrawer-brightgreen.svg?style=flat)](https://android-arsenal.com/details/1/1042)
+
+Android drawer icon with material design animation
+
+Note
+--------
+Basically same as appcompat_v7 version 21, you can use appcompat_v7
+```groovy
+compile 'com.android.support:appcompat-v7:21.0.+'
+```
+```xml
+<style name="AppTheme" parent="Theme.AppCompat.Light">
+    <item name="actionBarStyle">@style/ActionBar</item>
+</style>
+
+<style name="ActionBar" parent="Theme.AppCompat.Light.DarkActionBar">
+    <item name="drawerArrowStyle">@style/AppTheme.DrawerArrowToggle</item>
+</style>
+
+<style name="AppTheme.DrawerArrowToggle" parent="Base.Widget.AppCompat.DrawerArrowToggle">
+    <item name="color">@android:color/white</item>
+</style>
+```
+
+
+
+Download
+--------
+
+via Maven:
+```xml
+<dependency>
+  <groupId>com.ikimuhendis</groupId>
+  <artifactId>ldrawer</artifactId>
+  <version>0.1</version>
+</dependency>
+```
+or Gradle:
+```groovy
+compile 'com.ikimuhendis:ldrawer:0.1'
+```
+
+![alt tag](https://raw.githubusercontent.com/IkiMuhendis/LDrawer/master/images/animated.gif)
+
+##Usage
+
+You can use like using [normal drawer][2], instead of using `android.support.v4.app.ActionBarDrawerToggle` use `com.ikimuhendis.ldrawer.ActionBarDrawerToggle`
+
+First create `drawerArrow`
+
+```java
+drawerArrow = new DrawerArrowDrawable(this) {
+        @Override
+        public boolean isLayoutRtl() {
+            return false;
+        }
+    };
+```
+Then create `ActionBarDrawerToggle`
+
+```java
+mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+        drawerArrow, R.string.drawer_open,
+        R.string.drawer_close) {
+
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            invalidateOptionsMenu();
+        }
+
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            invalidateOptionsMenu();
+        }
+    };
+```
+You can stop or start animation
+```java
+mDrawerToggle.setAnimateEnabled(false);
+```
+You can set `drawerArrow` progress or change color
+```java
+drawerArrow.setProgress(0f); // normal position
+drawerArrow.setProgress(1f); // back arrow position
+drawerArrow.setColor(R.color.ldrawer_color); // to set color
+```
+##Example Projects
+
+  
+  [![Google Play](http://developer.android.com/images/brand/en_generic_rgb_wo_45.png)](https://play.google.com/store/apps/details?id=com.ikimuhendis.ldrawer.sample)
+  [LDrawer][3]
+  
+  
+  [![Google Play](http://developer.android.com/images/brand/en_generic_rgb_wo_45.png)](https://play.google.com/store/apps/details?id=com.ikimuhendis.android.foodjob)
+  [Yemek Tarifleri][4]
+  
+  [![Google Play](http://developer.android.com/images/brand/en_generic_rgb_wo_45.png)](https://play.google.com/store/apps/details?id=cz.kinst.jakub.asparagus)
+  [Asparagus - My Recipes][5]
+
+License
+=======
+
+    Copyright 2014 İkimühendis
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    
+    
+[1]: https://github.com/IkiMuhendis/LDrawer
+[2]: http://developer.android.com/training/implementing-navigation/nav-drawer.html
+[3]: https://play.google.com/store/apps/details?id=com.ikimuhendis.ldrawer.sample
+[4]: https://play.google.com/store/apps/details?id=com.ikimuhendis.android.foodjob
+[5]: https://play.google.com/store/apps/details?id=cz.kinst.jakub.asparagus
---
+++ sample/src/main/java/com/ikimuhendis/ldrawer/sample/SampleActivity.java
@@ -0,0 +1,197 @@
+package com.ikimuhendis.ldrawer.sample;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
+import com.ikimuhendis.ldrawer.DrawerArrowDrawable;
+
+import java.util.Locale;
+
+
+public class SampleActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerArrowDrawable drawerArrow;
+    private boolean drawerArrowColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+        ActionBar ab = getActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navdrawer);
+
+
+        drawerArrow = new DrawerArrowDrawable(this) {
+            @Override
+            public boolean isLayoutRtl() {
+                return false;
+            }
+        };
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+            drawerArrow, R.string.drawer_open,
+            R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+
+        String[] values = new String[]{
+            "Stop Animation (Back icon)",
+            "Stop Animation (Home icon)",
+            "Start Animation",
+            "Change Color",
+            "GitHub Page",
+            "Share",
+            "Rate"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        mDrawerToggle.setAnimateEnabled(false);
+                        drawerArrow.setProgress(1f);
+                        break;
+                    case 1:
+                        mDrawerToggle.setAnimateEnabled(false);
+                        drawerArrow.setProgress(0f);
+                        break;
+                    case 2:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        mDrawerToggle.syncState();
+                        break;
+                    case 3:
+                        if (drawerArrowColor) {
+                            drawerArrowColor = false;
+                            drawerArrow.setColor(getResources().getColor(R.color.ldrawer_color));
+                        } else {
+                            drawerArrowColor = true;
+                            drawerArrow.setColor(getResources().getColor(R.color.drawer_arrow_second_color));
+                        }
+                        mDrawerToggle.syncState();
+                        break;
+                    case 4:
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/IkiMuhendis/LDrawer"));
+                        startActivity(browserIntent);
+                        break;
+                    case 5:
+                        Intent share = new Intent(Intent.ACTION_SEND);
+                        share.setType("text/plain");
+                        share.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        share.putExtra(Intent.EXTRA_SUBJECT,
+                            getString(R.string.app_name));
+                        share.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_description) + "\n" +
+                            "GitHub Page :  https://github.com/IkiMuhendis/LDrawer\n" +
+                            "Sample App : https://play.google.com/store/apps/details?id=" +
+                            getPackageName());
+                        startActivity(Intent.createChooser(share,
+                            getString(R.string.app_name)));
+                        break;
+                    case 6:
+//                        String appUrl = "https://play.google.com/store/apps/details?id=" + getPackageName();
+//                        Intent rateIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(appUrl));
+//                        startActivity(rateIntent);
+                        Fragment fragment = new PlanetFragment();
+                        Bundle args = new Bundle();
+                        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+                        fragment.setArguments(args);
+
+                        FragmentManager fragmentManager = getFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                        // update selected item and title, then close the drawer
+                        mDrawerList.setItemChecked(position, true);
+                        mDrawerLayout.closeDrawer(mDrawerList);
+                        break;
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                mDrawerLayout.closeDrawer(mDrawerList);
+            } else {
+                mDrawerLayout.openDrawer(mDrawerList);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * Fragment that appears in the "content_frame", shows a planet
+     */
+    public static class PlanetFragment extends Fragment {
+        public static final String ARG_PLANET_NUMBER = "planet_number";
+
+        public PlanetFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+            int i = getArguments().getInt(ARG_PLANET_NUMBER);
+            String planet = getResources().getStringArray(R.array.planets_array)[i];
+
+            int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
+                    "drawable", getActivity().getPackageName());
+            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+            getActivity().setTitle(planet);
+            return rootView;
+        }
+    }
+}
