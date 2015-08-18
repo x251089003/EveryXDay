4450e01addcf27ba0c9ed7ad2a036c7b0d4acd65
XinxinLi <251089003@qq.com>
2015-08-18 21:53:01 +0800

    设置页相关功能

---
+++ EveryXDay/src/main/java/com/xinxin/everyxday/activity/ServiceTermActivity.java
@@ -0,0 +1,67 @@
+package com.xinxin.everyxday.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.xinxin.everyxday.R;
+import com.xinxin.everyxday.widget.swipeback.SwipeBackSherlockActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+/**
+ * Created by mengxiao on 15/8/18.
+ */
+public class ServiceTermActivity extends SwipeBackSherlockActivity implements View.OnClickListener {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setNavigationIcon(getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha));
+        mToolbar.setTitle("服务条款");// 标题的文字需在setSupportActionBar之前，不然会无效
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServiceTermActivity.this.finish();
+            }
+        });
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                this.finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+}
--- EveryXDay/src/main/java/com/xinxin/everyxday/fragment/FragmentSetting.java
+++ EveryXDay/src/main/java/com/xinxin/everyxday/fragment/FragmentSetting.java
@@ -2,15 +2,23 @@ package com.xinxin.everyxday.fragment;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.andexert.library.RippleView;
 import com.xinxin.everyxday.R;
 import com.xinxin.everyxday.activity.ContactUsActivity;
+import com.xinxin.everyxday.activity.OpenSourceActivity;
 import com.xinxin.everyxday.activity.QuestionActivity;
+import com.xinxin.everyxday.activity.ServiceTermActivity;
+import com.xinxin.everyxday.util.DataCleanUtil;
+import com.xinxin.everyxday.util.LocalStorageUtil;
+
+import java.io.File;
 
 /**
  * Created by xinxin on 15/7/28.
@@ -20,6 +28,15 @@ public class FragmentSetting extends Fragment{
     private View settingView;
     private RippleView question;
     private RippleView contact;
+    private RippleView serviceTerm;
+    private RippleView openSource;
+    private RippleView score;
+    private RippleView clear;
+    private RippleView checkUpdate;
+
+    private TextView cacheSize;
+
+    private LocalStorageUtil mLocalStorageUtil;
 
     public static FragmentSetting newInstance(Bundle args) {
         FragmentSetting myFragment = new FragmentSetting();
@@ -30,11 +47,20 @@ public class FragmentSetting extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocalStorageUtil = new LocalStorageUtil();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         settingView = inflater.inflate(R.layout.setting, null);
+
+        cacheSize = (TextView)settingView.findViewById(R.id.clear_mb);
+        try {
+            cacheSize.setText(DataCleanUtil.getCacheSize(new File(mLocalStorageUtil.getAppDir())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         question = (RippleView)settingView.findViewById(R.id.question);
         question.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
             @Override
@@ -52,6 +78,54 @@ public class FragmentSetting extends Fragment{
                 startActivity(intent);
             }
         });
+
+        serviceTerm = (RippleView)settingView.findViewById(R.id.service);
+        serviceTerm.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+                Intent intent = new Intent(getActivity(), ServiceTermActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        openSource = (RippleView)settingView.findViewById(R.id.opensource);
+        openSource.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+                Intent intent = new Intent(getActivity(), OpenSourceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        score = (RippleView)settingView.findViewById(R.id.mark);
+        score.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+                Uri uri = Uri.parse("market://details?id=" + getActivity().getPackageName());
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        clear = (RippleView)settingView.findViewById(R.id.clear);
+        clear.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+                DataCleanUtil.cleanCustomCache(mLocalStorageUtil.getImageCacheDir());
+                cacheSize.setText("0.0KB");
+            }
+        });
+
+        checkUpdate = (RippleView)settingView.findViewById(R.id.checkupdate);
+        checkUpdate.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+                Intent intent = new Intent(getActivity(), ContactUsActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return settingView;
     }
 
