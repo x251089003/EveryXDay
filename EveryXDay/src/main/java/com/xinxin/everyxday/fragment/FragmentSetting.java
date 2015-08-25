5dab380457712758ff99b3447b854dcdd014029c
lixinxin <251089003@qq.com>
2015-08-25 18:03:57 +0800

    检查更新（友盟）

--- EveryXDay/src/main/java/com/xinxin/everyxday/fragment/FragmentSetting.java
+++ EveryXDay/src/main/java/com/xinxin/everyxday/fragment/FragmentSetting.java
@@ -8,8 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andexert.library.RippleView;
+import com.gc.materialdesign.widgets.Dialog;
+import com.nispok.snackbar.Snackbar;
+import com.umeng.update.UmengUpdateAgent;
+import com.umeng.update.UmengUpdateListener;
+import com.umeng.update.UpdateResponse;
+import com.umeng.update.UpdateStatus;
 import com.xinxin.everyxday.R;
 import com.xinxin.everyxday.activity.ContactUsActivity;
 import com.xinxin.everyxday.activity.OpenSourceActivity;
@@ -114,6 +121,11 @@ public class FragmentSetting extends Fragment{
             public void onComplete(RippleView rippleView) {
                 DataCleanUtil.cleanCustomCache(mLocalStorageUtil.getImageCacheDir());
                 cacheSize.setText("0.0KB");
+                Snackbar.with(getActivity()) // context
+                        .colorResource(R.color.app_main_theme_color_transparent)
+                        .duration(Snackbar.SnackbarDuration.LENGTH_SHORT)
+                        .text("缓存清除成功") // text to display
+                        .show(getActivity());
             }
         });
 
@@ -121,8 +133,45 @@ public class FragmentSetting extends Fragment{
         checkUpdate.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
             @Override
             public void onComplete(RippleView rippleView) {
-                Intent intent = new Intent(getActivity(), ContactUsActivity.class);
-                startActivity(intent);
+                UmengUpdateAgent.setDefault();
+                UmengUpdateAgent.setUpdateAutoPopup(false);
+                UmengUpdateAgent.setUpdateOnlyWifi(false);
+                UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
+                    @Override
+                    public void onUpdateReturned(int updateStatus,UpdateResponse updateInfo) {
+                        switch (updateStatus) {
+                            case UpdateStatus.Yes: // has update
+                                UmengUpdateAgent.showUpdateDialog(getActivity(), updateInfo);
+                                Dialog dialog = new Dialog(getActivity(),"NEW新版本", updateInfo.updateLog);
+                                dialog.getButtonAccept().setText("确定");
+                                dialog.getButtonCancel().setText("取消");
+                                dialog.show();
+                                break;
+                            case UpdateStatus.No: // has no update
+                                Snackbar.with(getActivity()) // context
+                                        .colorResource(R.color.app_main_theme_color_transparent)
+                                        .duration(Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                        .text("当前已是最新版本") // text to display
+                                        .show(getActivity());
+                                break;
+                            case UpdateStatus.NoneWifi: // none wifi
+                                Snackbar.with(getActivity()) // context
+                                        .colorResource(R.color.app_main_theme_color_transparent)
+                                        .duration(Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                        .text("仅在WIFI环境下更新") // text to display
+                                        .show(getActivity());
+                                break;
+                            case UpdateStatus.Timeout: // time out
+                                Snackbar.with(getActivity()) // context
+                                        .colorResource(R.color.app_main_theme_color_transparent)
+                                        .duration(Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                        .text("网络连接失败") // text to display
+                                        .show(getActivity());
+                                break;
+                        }
+                    }
+                });
+                UmengUpdateAgent.update(getActivity());
             }
         });
 
